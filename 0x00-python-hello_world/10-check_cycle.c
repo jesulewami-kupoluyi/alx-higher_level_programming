2e35f8d07834d83d896d03abab6570172e3aae8b
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2023-01-03 06:32:13 -0800

    update

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<lists.h>
+/**
+ * list:inputs values
+ * int check_cycle: function that prints numbers
+ * return:0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+list 
+	list1 -> list;
+	list2 -> list1;
+}
