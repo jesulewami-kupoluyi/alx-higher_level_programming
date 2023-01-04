d6a70a51c0d6660f98df67bae8e6e5945a0ea650
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2023-01-04 07:45:57 -0800

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for digit1 in range(0, 9):
-    for digit2 in range(1, 10):
-        print("{}{}".format(digit1, digit2), end=", ")
+    for digit2 in range(digit1 + 1, 10):
+        if digit == 8 and digit2 == 9:
+        print("{}{}".format(digit1, digit2))
